18536ad4781ecf82a15af42f07dfa067c604bc47
Oliver Abraham <mail@oliver-abraham.de>
2023-10-15 19:06:30 +0200

    Data object value translation

--- ModText/ModText.cs
+++ ModText/ModText.cs
@@ -1,19 +1,21 @@
-﻿using HomenetBase;
-using Newtonsoft.Json;
-using System;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
-using System.DirectoryServices.ActiveDirectory;
 using System.Windows.Controls;
 
 namespace AllOnOnePage.Plugins
 {
-	public class ModText : ModBase, IPlugin
+    public class ModText : ModBase, IPlugin
 	{
 		#region ------------- Settings ------------------------------------------------------------
 		public class MyConfiguration : ModuleSpecificConfig
 		{
-			public string Text				{ get; set; }
-			public string ServerDataObject  { get; set; }
+			public string Text				  { get; set; }
+			public string ServerDataObject    { get; set; }
+			public string ServerMessages      { get; set; }
+			public string ServerFadeOutValues { get; set; }
+			public string ServerPlaySound     { get; set; }
+			public string FadeOutAfter        { get; set; }
 		}
 		#endregion
 
@@ -21,12 +23,16 @@ namespace AllOnOnePage.Plugins
 
 		#region ------------- Fields --------------------------------------------------------------
 		private MyConfiguration _myConfiguration;
-		#endregion
+        private Params _serverMessages;
+        private Params _serverFadeOutValues;
+        private Params _serverPlaySound;
+        private Params _fadeOutAfter;
+        #endregion
 
 
 
-		#region ------------- Init ----------------------------------------------------------------
-		public override void Init(ModuleConfig config, Grid parent, System.Windows.Threading.Dispatcher dispatcher)
+        #region ------------- Init ----------------------------------------------------------------
+        public override void Init(ModuleConfig config, Grid parent, System.Windows.Threading.Dispatcher dispatcher)
 		{
 			base.Init(config, parent, dispatcher);
 			InitConfiguration();
@@ -75,7 +81,7 @@ namespace AllOnOnePage.Plugins
 				{
 					if (dataObject.Name != _myConfiguration.ServerDataObject)
 						return;
-					Value = dataObject.Value;
+					Value = MapTechnicalValueToDisplayValue(dataObject.Value);
 				}
 				else
 					Value = ReadValueFromHomeAutomationServer();
@@ -85,7 +91,7 @@ namespace AllOnOnePage.Plugins
             SetValueControlVisible();
         }
 
-		public override Dictionary<string,string> GetHelp()
+        public override Dictionary<string,string> GetHelp()
 		{
             var texts = new Dictionary<string,string>();
             texts.Add("de-DE", 
@@ -110,6 +116,11 @@ In den allgemeinen Einstellungen im Feld 'Text' kann der Text eingegeben werden.
 
 			if (_myConfiguration == null)
 				CreateSeedData();
+
+			_serverMessages      = Deserialize(_myConfiguration.ServerMessages);
+			_serverFadeOutValues = Deserialize(_myConfiguration.ServerFadeOutValues);
+			_serverPlaySound     = Deserialize(_myConfiguration.ServerPlaySound);
+			_fadeOutAfter        = Deserialize(_myConfiguration.FadeOutAfter);
 		}
 
         private string ReadValueFromHomeAutomationServer()
@@ -124,6 +135,55 @@ In den allgemeinen Einstellungen im Feld 'Text' kann der Text eingegeben werden.
 				return "???";
 			}
         }
+
+        private Params Deserialize(string data)
+        {
+			if (string.IsNullOrWhiteSpace(data))
+				return null;
+
+            var parts = data.Split('|', StringSplitOptions.RemoveEmptyEntries);
+			var result = new Params();
+            foreach (var part in parts)
+			{
+				var pair = data.Split('=', StringSplitOptions.RemoveEmptyEntries);
+				result.Values.Add(new Param(pair[0], pair[1]));
+			}
+			return result;
+        }
+
+        private string MapTechnicalValueToDisplayValue(string value)
+        {
+			if (_serverMessages is null)
+				return value;
+
+            var message = _serverMessages.Values.Where(x => x.Value == value).FirstOrDefault();
+			if (message is not null)
+                return message.Text;
+			else
+				return value;
+        }
         #endregion
 	}
+
+    internal class Params
+    {
+		public List<Param> Values { get; set; }
+
+        public Params()
+        {
+			Values = new List<Param>();
+        }
+    }
+
+    public class Param
+    {
+        public string Value { get; set; }
+		public string Text  { get; set; }
+
+        public Param(string value, string text)
+        {
+			Value = value;
+            Text  = text;
+        }
+    }
 }
